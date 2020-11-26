92967eb4d197ab035bd09735cc9ea26d8f326c9f
prachi sharma <67380116+prachi0603@users.noreply.github.com>
2020-11-26 15:13:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,57 +1,70 @@
-var bullet,wall,thickness;
-
-var speed, weight; 
-
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
   createCanvas(1600, 400);
 
+  speed=random(223,321)
+  weight=random(30,52)
 
-	speed=random(223,321)
-	weight=random(30,52)
-    thickness = random(22,83)
 
-	bullet=createSprite(50, 200, 50,50);   
+  thickness=random(22,83)
 
-	bullet.velocityX = speed;
 
-	bullet.shapeColor=color(255);
 
 
   
-  	wall=createSprite(1200,200, thickness, height/2)
-  	wall.shapeColor=color(80,80,80)
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(230,230,230);
+  wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
-  background(0);
-if(hasCollided(bullet,wall))
+  background(0);  
+ 
+  if(hasCollided(bullet, wall))
   {
-	  bullet.velocityX = 0;
-	  var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness)
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-if(damage>10)
-{
-	wall.shapeColor = color(255,0,0);
-}
-if(damage<10)
-{
-	wall.shapeColor = color(0,255,0);
-}
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
+
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
-   
-  
+
+
   drawSprites();
  
 }
-function hasCollided(bullet,wall)
+
+
+function hasCollided(bullet, wall)
 {
-  bulletRightEdge = bullet.x + bullet.width;
-wallLeftEdge = wall.x
-if (bulletRightEdge> = wallLeftEdge)
-{
-	return true 
-}
-return false;
+	bulletRightEdge=bullet.x +bullet.width;
+	wallLeftEdge=wall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
